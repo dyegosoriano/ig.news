1f26b37ef3ad838b5f5e0f9b47c69e06178ea6bd
Dyego Soriano <dyegodf2@gmail.com>
2022-04-12 21:48:08 -0300

    refactor: finalizing base structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-import styled from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
-  return <h1 className={styled.title}>Hello World</h1>
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  )
 }
