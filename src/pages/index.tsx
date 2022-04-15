e43f2f75534b4a9e9124ba2fce29d250c164e6ad
Dyego Soriano <dyegodf2@gmail.com>
2022-04-15 16:50:10 -0300

    feat: using Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -42,7 +42,7 @@ export default function Home({ product }: IHomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Koso3HoZ6nXHMb7brQG4xPw')
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 
   return {
+    revalidate: 60 * 60 * 24, // 24 hours
     props: { product }
   }
 }
