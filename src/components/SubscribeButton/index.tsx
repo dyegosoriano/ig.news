069ab03f74f8d873cfbee6a220be367464d6bf30
Dyego Soriano <dyegodf2@gmail.com>
2022-05-24 20:53:45 -0300

    feat: add payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/react'
+
+import getStripeJs from '../../services/stripe-js'
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss'
 
 interface ISubscribeProps {
@@ -5,8 +10,28 @@ interface ISubscribeProps {
 }
 
 const SubscribeButton = ({ priceId }: ISubscribeProps) => {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const { data } = await api.post('/subscribe')
+      const { sessionId } = data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button className={styles.subscribeButton} type="button">
+    <button className={styles.subscribeButton} onClick={handleSubscribe} type="button">
       Subscribe now
     </button>
   )
